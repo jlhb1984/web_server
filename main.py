aa7608ee730c292d1fb76ba6229887a379cb6355
jlhb1984 <jlhb1984@gmail.com>
2024-11-06 20:53:31 -0500

    Se agregó el servicio con FastAPI y Uvicorn.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+#Si quiero agregar una respuesta como html:
+from fastapi.responses import HTMLResponse
+
+app=FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    #return {'name': 'Platzi'}
+    return """
+        <h1>Hola soy una pagina web</h>
+        <p>soy un párrafo.</p>
+        """
 
 def run():
     store.get_categories()
